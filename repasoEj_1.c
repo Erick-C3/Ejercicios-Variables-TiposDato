13510b7babcec9bfe0f3907604381f520b8543be
Erick-C3 <81482790+Erick-C3@users.noreply.github.com>
2021-06-04 13:17:50 -0300

    Add files via upload

---
+++ repasoEj_1.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/*
+	Crear 2 variables de tipo de dato entero
+	imprimir un mensaje que contenga los valores contenidos en las variables
+*/
+
+
+int main(){ // funcion main - subalgoritmo - algoritmo
+	
+	
+	int primerVariable = 0;//definir una variable de tipo de dato entero
+	int segundaVariable = 3;
+
+	printf("segunda variable: %i - primer variable: %i", primerVariable, segundaVariable);
+	
+
+	return 0;
+}
+
